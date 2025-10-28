1933cc4e61fe67fda0f57480f557cc18929a59f9
Mohamed Nihal <nihalbinnowfal@gmail.com>
2025-10-28 16:03:01 +0530

    updated

--- 6.single_linked_list.c
+++ 6.single_linked_list.c
@@ -9,18 +9,84 @@ struct node
 
 struct node *header = NULL;
 
-void createnode()
+
+/*
+void createNode(int item)
 {
 	struct node *newnode;
 	newnode = (struct node *)malloc(sizeof(struct node));
-	newnode->data = 10;
+	newnode->data = item;
 	newnode->link = NULL;
-	header=newnode;
 }
+*/
+
+
+void insertAtFront(int item)
+{
+        struct node *newnode;
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->data = item;
+        newnode->link = NULL;
+
+	if(header==NULL)
+	{
+		header=newnode;
+	}
+	else
+	{
+		newnode->link=header;
+		header=newnode;
+	}
+}
+
+void insertAtLast(int item)
+{
+        struct node *newnode;
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->data = item;
+        newnode->link = NULL;
+
+        if(header==NULL)
+        {
+                header=newnode;
+        }
+	else
+	{
+		struct node *ptr;
+        	ptr=header;
+        	while(ptr->link !=NULL)
+        	{
+                	ptr=ptr->link;
+        	}
+		ptr->link=newnode;
+	}
+}
+
+
+
+void traverseList()
+{
+	struct node *ptr;
+	ptr=header;
+	while(ptr!=NULL)
+	{
+			printf("%d\t",ptr->data);
+			ptr=ptr->link;
+	}
+	printf("\n");
+}
+
 
 int main()
 {
-	createnode();
-	printf("Data: %d\n",header->data);
+	insertAtFront(10);
+        insertAtFront(20);
+        insertAtFront(30);
+        insertAtFront(40);
+	insertAtLast(40);
+	insertAtLast(40);
+
+
+	traverseList();
 	return (0);
 }
